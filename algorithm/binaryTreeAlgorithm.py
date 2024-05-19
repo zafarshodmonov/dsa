24e453adbbfffd94fa5038e21c62fcbb3c32546d
zafarshodmonov <zafarshodmonov1996@gmail.com>
2024-05-19 21:39:13 +0500

    Z

--- algorithm/binaryTreeAlgorithm.py
+++ algorithm/binaryTreeAlgorithm.py
@@ -9,6 +9,117 @@ class TreeNode:
 
 class BinaryTreeAlgorithm:
 
+    def F106Y(self, ino: list[int], post: list[int]):
+        root = post[-1]
+        if len(post) == 1:
+            return root, [], [], [], []
+        
+        ele = post[-1]
+        ind = ino.index(ele)
+        A = (ind == 0)
+        B = (ind == len(ino) - 1)
+        if A and B:
+            pass
+        elif A and not B:
+            re_ch_ino = []
+            re_ch_post = []
+            re_on_ino = ino[ind+1:]
+            re_on_post = post[:-1]
+            return root, re_ch_ino, re_ch_post, re_on_ino, re_on_post
+        elif not A and B:
+            re_ch_ino = ino[:ind]
+            re_ch_post = post[:-1]
+            re_on_ino = 0
+            re_on_post = 0
+            return root, re_ch_ino, re_ch_post, re_on_ino, re_on_post
+        else:
+            re_ch_ino = ino[:ind]
+            re_ch_post = post[:ind]
+            re_on_ino = ino[ind+1:]
+            re_on_post = post[ind:-1]
+            return root, re_ch_ino, re_ch_post, re_on_ino, re_on_post
+
+    def inPostToBinaryTree(self, inorder: list[int], postorder: list[int]):
+        def B(ino, post):
+            if not post:
+                return None
+            res = self.F106Y(ino, post)
+            l = B(res[1], res[2])
+            r = B(res[3], res[4])
+            return TreeNode(res[0], l, r)
+        return B(inorder, postorder)
+
+    def constructFromPrePostF(self, pre: list[int], post: list[int]):
+        if len(pre) == 1:
+            return pre[0], [], [], [], []
+        ele = pre[1]
+        ind = post.index(ele)
+        B = (ind == len(post) - 2)
+        if B:
+            re_ch_pre = []
+            re_ch_post = []
+            re_on_pre = pre[1:]
+            re_on_post = post[:-1]
+            return pre[0], re_ch_pre, re_ch_post, re_on_pre, re_on_post
+        re_ch_pre = pre[1:ind+2]
+        re_ch_post = post[:ind+1]
+        re_on_pre = pre[ind+2:]
+        re_on_post = post[ind+1:-1]
+        return pre[0], re_ch_pre, re_ch_post, re_on_pre, re_on_post
+
+    def prePostToBinaryTree(self, preorder: list[int], postorder: list[int]):
+        def B(pre, post):
+            if not pre:
+                return None
+            res = self.constructFromPrePostF(pre, post)
+            l = B(res[1], res[2])
+            r = B(res[3], res[4])
+            return TreeNode(res[0], l, r)
+        return B(preorder, postorder)
+        
+    def preInToBinaryTreeY(self, pre: list[int], ino: list[int]):
+
+        root_val = pre[0]
+        ind = ino.index(root_val)
+
+        A = (ind == 0)
+        B = (ind == len(ino) - 1)
+
+        if A and B:
+            re_ch_ino = []
+            re_ch_pre = []
+            re_on_ino = []
+            re_on_pre = []
+            return root_val, re_ch_ino, re_ch_pre, re_on_ino, re_on_pre
+        elif A and not B:
+            re_ch_ino = []
+            re_ch_pre = []
+            re_on_ino = ino[1:]
+            re_on_pre = pre[1:]
+            return root_val, re_ch_ino, re_ch_pre, re_on_ino, re_on_pre
+        elif not A and B:
+            re_ch_ino = ino[:-1]
+            re_ch_pre = pre[1:]
+            re_on_ino = []
+            re_on_pre = []
+            return root_val, re_ch_ino, re_ch_pre, re_on_ino, re_on_pre
+        else:
+            re_ch_ino = ino[:ind]
+            re_ch_pre = pre[1:ind + 1]
+            re_on_ino = ino[ind + 1:]
+            re_on_pre = pre[ind + 1:]
+            return root_val, re_ch_ino, re_ch_pre, re_on_ino, re_on_pre
+
+    def preInToBinaryTree(self, preorder: list[int], inorder: list[int]):
+        def B(pre, ino):
+            if not pre:
+                return None
+            res = self.preInToBinaryTreeY(pre, ino)
+            l = B(res[2], res[1])
+            r = B(res[4], res[3])
+            return TreeNode(res[0], l, r)
+        return B(preorder, inorder)
+    
 
     def binaryTreeToStr(self, root) -> str:
         
@@ -168,15 +279,30 @@ class BinaryTreeAlgorithm:
         return ans[0]
         
 
-
-
-
 def main():
-    root = TreeNode(1, TreeNode(2, TreeNode(4)), TreeNode(3))
     algo = BinaryTreeAlgorithm()
 
-    nums = [4, 7, 2, "-", "*", 5, "*"]
-    print(algo.F(nums))
+    # Input
+    str_tree = "1(2(4(7))(6()(9)))(3(21)(5()(10)))"
+    inorder = [7, 4, 2, 6, 9, 1, 21, 3, 5, 10]
+    preorder = [1, 2, 4, 7, 6, 9, 3, 21, 5, 10]
+    postorder = [7, 4, 9, 6, 2, 21, 10, 5, 3, 1]
+
+    # Processing
+    root = algo.preInToBinaryTree(preorder, inorder)
+
+    # Output
+    print("Str: ", algo.binaryTreeToStr(root))
+    print("Inorder: ", algo.binaryTreeToInorderTraversal(root))
+    print("Preorder: ", algo.binaryTreeToPreorderTraversal(root))
+    print("Postorder: ", algo.binaryTreeToPostorderTraversal(root))
+
+
+    # root = TreeNode(1, TreeNode(2, TreeNode(4)), TreeNode(3))
+    
+
+    # nums = [4, 7, 2, "-", "*", 5, "*"]
+    # print(algo.F(nums))
 
     # x = 20
     # y = 5
